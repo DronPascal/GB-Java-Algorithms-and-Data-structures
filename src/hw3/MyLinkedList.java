a536724ba398c95efb62bb5e91132057a0c0dc2e
Andrew <shpuntik0902@gmail.com>
2020-11-24 22:21:11 +0300

    hw3

--- src/hw3/MyLinkedList.java
+++ src/hw3/MyLinkedList.java
@@ -1,4 +1,115 @@
 package hw3;
 
-public class MyLinkedList {
+import java.util.AbstractList;
+import java.util.LinkedList;
+
+class ListElement<E> {
+    ListElement next;  // указатель на следующий элемент
+    E data;  // данные
+}
+
+class MyList<E> extends AbstractList {
+    private ListElement head;
+    private ListElement tail;
+
+    void addFront(E data) {  //добавить спереди
+        ListElement a = new ListElement();  //создаём новый элемент
+        a.data = data;
+        if (head == null) {  //если список пуст
+            head = a;
+            tail = a;
+        } else {
+            a.next = head;
+            head = a;
+        }
+    }
+
+    public boolean add(Object data) {  //добавление в конец списка
+        ListElement a = new ListElement();  //создаём новый элемент
+        a.data = data;
+        if (tail == null) {
+            head = a;
+            tail = a;
+        } else {
+            tail.next = a;
+            tail = a;
+        }
+        return true;
+    }
+
+    void printList() {
+        ListElement t = head;
+        while (t != null) {
+            System.out.print(t.data + " ");
+            t = t.next;
+        }
+    }
+
+    public boolean remove(Object data) { //удаление элемента
+        if (head == null)
+            return false;
+        if (head == tail) {
+            head = null;
+            tail = null;
+            return false;
+        }
+        if (head.data == data) {
+            head = head.next;
+            return false;
+        }
+        ListElement cur = head;
+        while (cur.next != null) {
+            if (cur.next.data == data) {
+                if (tail == cur.next)
+                    tail = cur;
+                cur.next = cur.next.next;
+                return false;}
+            cur = cur.next;
+        }
+        return true;
+    }
+
+    @Override
+    public Object get(int index) {
+        if (head == null) return null;
+        ListElement cur = head;
+        int count = 0;
+        while (count != index && cur.next != null) {
+            count++;
+            cur = cur.next;
+        }
+        return count == index ? cur.data : 0;
+    }
+
+    @Override
+    public int size() {
+        if (head == null) return 0;
+        ListElement cur = head;
+        int count = 1;
+        while (cur.next != null) {
+            count++;
+            cur = cur.next;
+        }
+        return count;
+    }
+
+    private class LinkedListIterator
+            implements java.util.Iterator<E> {
+        private ListElement<E> current = head;
+
+        @Override
+        public boolean hasNext() {
+            return (current != null);
+        }
+
+        @Override
+        public E next() {
+            E e = current.data;
+            current = current.next;
+            return e;
+        }
+    }
+}
+
+class My2LinkedList<E> extends LinkedList {
 }
--- src/hw3/hw3.java
+++ src/hw3/hw3.java
@@ -2,35 +2,120 @@ package hw3;
 
 import hw1.myInt;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
+
 
 public class hw3 {
-    /*
-Задание 3.2
-На основе списка из задания 3.1 реализуйте основные методы добавления, удаления и получения объекта или элемента из списка.
-Оценить выполненные методы с помощью базового класса System.nanoTime().
-Задание 3.3
-Реализуйте простой односвязный список и его базовые методы.
-Задание 3.4
-На основе списка из задания 3.1 реализуйте простой двусторонний список и его базовые методы.
-Реализуйте список заполненный объектами из вашего класса из задания 1.3
-Задание 3.5
-Реализуйте итератор на основе связанных списков из задания 3.4 и выполните базовые операции итератора.
-Оцените время выполнения операций с помощью базового метода System.nanoTime()
-
-     */
     public static void main(String[] args) {
       /*
-        Задание 3.1
-        На основе массива из домашнего задания 2.1 реализуйте простой список и коллекцию.
-        Оцените время выполнения преобразования.
+      Задание 3.1
+            На основе массива из домашнего задания 2.1 реализуйте простой список и коллекцию.
+            Оцените время выполнения преобразования.
        */
-        ArrayList<myInt> arr = new ArrayList<>();
+        Integer[] arr = new Integer[20];
         Random random = new Random();
         for (int i = 0; i < arr.length; i++)
-            arr.add(random.nextInt());
+            arr[i] = random.nextInt(100);
 
-        for (myInt arr : ArrayList)
+        //нерасширяемый список, arr используется как модель, на него ссылаются, а не копируют.
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList = Arrays.asList(arr);
+
+        //копировать надо так. arrList - самостоятельный объект
+        ArrayList<Integer> arrList = new ArrayList<>();
+        long start = System.nanoTime();
+        Collections.addAll(arrList, arr);
+        long stop = System.nanoTime();
+        System.out.println("Collections.addAll(arrList, arr) done in " + (stop - start) + "ns");
+
+        //“Коллекции” — общее название для нескольких структур данных в Java. Данные можно хранить многими разными способами.
+        //Список реализвал, реализую тогда еще LinkedList
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        start = System.nanoTime();
+        Collections.addAll(linkedList, arr);
+        stop = System.nanoTime();
+        System.out.println("Collections.addAll(linkedList, arr) done in " + (stop - start) + "ns");
+
+        /*
+        Задание 3.2
+              На основе списка из задания 3.1 реализуйте основные методы добавления, удаления и получения объекта или элемента из списка.
+              Оценить выполненные методы с помощью базового класса System.nanoTime().
+         */
+        //добавление в конец
+        System.out.println("\n" + arrList.toString());
+        start = System.nanoTime();
+        arrList.add(200);
+        stop = System.nanoTime();
+        System.out.println("arrList.add(200) done in " + (stop - start) + "ns " + arrList.toString());
+        //добавление в центр
+        start = System.nanoTime();
+        arrList.add(10, 200);
+        stop = System.nanoTime();
+        System.out.println("arrList.add(10, 200) done in " + (stop - start) + "ns " + arrList.toString());
+        //удаление из конца
+        start = System.nanoTime();
+        arrList.remove(21);
+        stop = System.nanoTime();
+        System.out.println("arrList.remove(21) done in " + (stop - start) + "ns " + arrList.toString());
+        //удаление из середины
+        start = System.nanoTime();
+        arrList.remove(10);
+        stop = System.nanoTime();
+        System.out.println("arrList.remove(10) done in " + (stop - start) + "ns " + arrList.toString());
+        //получение по индексу
+        start = System.nanoTime();
+        arrList.get(10);
+        stop = System.nanoTime();
+        System.out.println(arrList.get(10) + " arrList.get(10) done in " + (stop - start) + "ns");
+
+        /*
+        Задание 3.3
+              Реализуйте простой односвязный список и его базовые методы.
+         */
+        MyList<Integer> myList = new MyList<Integer>();
+        myList.add(43);
+        myList.add(54);
+        myList.remove(myList.get(0));
+        System.out.println(myList.get(0));
+        /*
+        Задание 3.4
+            На основе списка из задания 3.1 реализуйте простой двусторонний список и его базовые методы.
+            Реализуйте список заполненный объектами из вашего класса из задания 1.3
+         */
+        My2LinkedList<Integer> my2LinkedList = new My2LinkedList<Integer>();
+        my2LinkedList.add(new myInt(21));
+        my2LinkedList.add(0, new myInt(1));
+        my2LinkedList.remove(new myInt(1));
+        System.out.println(my2LinkedList.get(0));
+        /*
+        Задание 3.5
+            Реализуйте итератор на основе связанных списков из задания 3.4 и выполните базовые операции итератора.
+            Оцените время выполнения операций с помощью базового метода System.nanoTime()
+         */
+        MyList<String> letters = new MyList<String>();
+        letters.add("a");
+        letters.add("b");
+        letters.add("c");
+        letters.add("d");
+
+        Iterator<String> iter = letters.iterator();
+        while (iter.hasNext()) {
+            System.out.print(iter.next() + " ");
+        }
+
+        iter = letters.iterator();
+        //hasNext
+        start = System.nanoTime();
+        iter.hasNext();
+        stop = System.nanoTime();
+        System.out.println("\niter.hasNext() done in " + (stop - start) + "ns ");
+        //remove
+        start = System.nanoTime();
+        iter.next();
+        stop = System.nanoTime();
+        System.out.println("iter.next() done in " + (stop - start) + "ns ");
+
     }
 }
+
+
